ffd2c3c7aceb7ce8f8f2644a55bc1eb6d58bd330
Gerald Versluis <gerald@verslu.is>
2020-10-15 16:46:18 +0200

    Initial commit

---
+++ XFEUnitConvertersSample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFEUnitConvertersSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var kilometers = Convert.ToDouble(UnitEntry.Text);
+            var result = UnitConverters.KilometersToMiles(kilometers);
+
+            ResultLabel.Text = result.ToString();
+        }
+    }
+}
